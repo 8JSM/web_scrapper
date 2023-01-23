252b5a3fdcc004bd21d3efcedb01ef4fbb41272b
8JSM <8jadeshape@gmail.com>
2023-01-23 20:51:04 +0900

    2023/01/23/makingBot

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 from requests import get
 from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+# jobs = extract_wwr_jobs("python")
+# print(jobs)
+results = []
+driver = webdriver.Chrome()
+driver.get("http://kr.indeed.com/jobs?q=python")
+soup = BeautifulSoup(driver.page_source, "html.parser")
+job_list = soup.find("ul", class_="jobsearch-ResultList")
+jobs = job_list.find_all('li', recursive=False)
+for job in jobs:
+    zone = job.find("div", class_="mozaic-zone")
+    if zone == None:
+        anchor = job.select_one("h2 a")
+        title = anchor['aria-label']
+        link = anchor['href']
+        company = job.find("span", class_="companyName")
+        location = job.find("div", class_="companyLocation")
 
-base_url = "https://weworkremotely.com/remote-jobs/search?term="
-search_term = "python"
-
-response = get(f"{base_url}{search_term}")
-if response.status_code != 200:
-    print("Can`t request website")
-else:
-    results = []
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find_all('section', class_="jobs")
-    for job_section in jobs:
-        job_list = job_section.find_all('li')
-        job_list.pop(-1)
-        for post in job_list:
-            anchors = post.find_all('a')
-            anchor = anchors[1]
-            link = anchor['href']
-            company, kind, region = anchor.find_all(
-                'span', class_="company")
-            title = anchor.find('span', class_="title")
-            print(company, kind, region, title)
-            print("----------------------")
-            job_data = {
-                "company": company.string,
-                "region": region.string,
-                "position": kind.string,
-            }
-            results.append(job_data)
-    for result in results:
-        print(result)
-        print("/////////")
+        job_data = {
+            "link": f"http://kr.indeed.com{link}",
+            "company": company.string,
+            "location": location.string,
+            "position": title,
+        }
+        results.append(job_data)
+while (True):
+    pass
