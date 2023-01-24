2d47bb542ac751f0b864dacce5d98c48e56c45b0
8JSM <8jadeshape@gmail.com>
2023-01-24 17:38:56 +0900

    2023/01/24/completePaginationAndIndeed

--- main.py
+++ main.py
@@ -4,27 +4,50 @@ from extractors.wwr import extract_wwr_jobs
 from selenium import webdriver
 # jobs = extract_wwr_jobs("python")
 # print(jobs)
-results = []
-driver = webdriver.Chrome()
-driver.get("http://kr.indeed.com/jobs?q=python")
-soup = BeautifulSoup(driver.page_source, "html.parser")
-job_list = soup.find("ul", class_="jobsearch-ResultList")
-jobs = job_list.find_all('li', recursive=False)
-for job in jobs:
-    zone = job.find("div", class_="mozaic-zone")
-    if zone == None:
-        anchor = job.select_one("h2 a")
-        title = anchor['aria-label']
-        link = anchor['href']
-        company = job.find("span", class_="companyName")
-        location = job.find("div", class_="companyLocation")
 
-        job_data = {
-            "link": f"http://kr.indeed.com{link}",
-            "company": company.string,
-            "location": location.string,
-            "position": title,
-        }
-        results.append(job_data)
-while (True):
-    pass
+
+def get_page_count(keyword):
+    driver = webdriver.Chrome()
+    base_url = "http://kr.indeed.com/jobs?q="
+    driver.get(f"{base_url}{keyword}")
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    pagination = soup.find("nav", attrs={"aria-label": "pagination"})
+    if pagination == None:
+        return 1
+    pages = pagination.find_all('div', recursive=False)
+    count = len(pages)
+    if count >= 5:
+        return 5
+    else:
+        return count
+
+
+def extract_indeed_jobs(keyword):
+    pages = get_page_count(keyword)
+    for page in range(pages):
+        results = []
+        driver = webdriver.Chrome()
+        base_url = "http://kr.indeed.com/jobs?q="
+        driver.get(f"{base_url}{keyword}")
+
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        job_list = soup.find("ul", class_="jobsearch-ResultList")
+        jobs = job_list.find_all('li', recursive=False)
+        for job in jobs:
+            zone = job.find("div", class_="mozaic-zone")
+            if zone == None:
+                anchor = job.select_one("h2 a")
+                title = anchor['aria-label']
+                link = anchor['href']
+                company = job.find("span", class_="companyName")
+                location = job.find("div", class_="companyLocation")
+
+                job_data = {
+                    "link": f"http://kr.indeed.com{link}",
+                    "company": company.string,
+                    "location": location.string,
+                    "position": title,
+                }
+                results.append(job_data)
+        while (True):
+            pass
